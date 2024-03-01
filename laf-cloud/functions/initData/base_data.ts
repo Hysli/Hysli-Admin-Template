c4e2f8567b5fa48103c58bfd56b5e4f17e93ec07
Jokeny <554707280@qq.com>
2024-03-02 00:19:29 +0800

    fix: 去除不必要的参数 (#2)

--- laf-cloud/functions/initData/base_data.ts
+++ laf-cloud/functions/initData/base_data.ts
@@ -55,16 +55,14 @@ const initData = [
         },
         pay: {
           wechat: {
-            wx_app_id: 'wx3f19bac6d9089727',
-            wx_app_secret: 'f6c961202989dcdf27eaff85804c2c6c',
-            app_id: 'wx6ff710afda480cd6',
-            app_secret: '579118da046a93c83580077c4b56e812',
-            mch_id: '1621656887',
-            mch_key: 'a4139958a4139958a4139958a4139958',
-            private_key:
-              '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2msJBre5qr9Z7\nb7hHliibpUdzPGPnsxIsdWgmGc/uV+bA6o44zyBM1DnwU4OTMVlbA7756DwkDcDK\nbWKYs1sNOiAnetfH0+zwiXea6MpkeFUHthRw2srSwRq7B9x7vVQrNCaDnIl5+eUh\ngqHTlkvJzw/sPfeQnm7UMSH6aksU0FvkCPGuT3xnjGi/5rPoskJE7Bhqb6EOZ1Sy\n4UyU4CS9uVL42rfkVr1Fl6b24p5vBg5rtDZSfU4hQv5bLdokBO1U1SFXxGKYCY/M\nh6H+x77p6zNyP6j5y8vtpx3XCOvTuJOcoJEMkdLj5PwofCrn+nBz0qQIuH3szWwm\n8hQfdYuJAgMBAAECggEBAII48YnHUTySM4IJ1UBwDHubLc43ZgPph29fNyvcwpV1\n6GDxpQVMaPuBaeRCmpWQ3ulyTXKJNMJ9cFysD2FYnvla121wRIwETXfogPxfQV7P\nFcyPBBrLXVNZVQfGfm1cQrcg1MqNCtmK5eW09FUhXOEJopBWTMO8yMrYUyPzQ6IO\nFOge6WySEEhdrRIu/Z/WqofJ/JSNIbILwZMRXnnWFLWJJQ0p38gLdOdrId8w7mTv\n5fhccZ4VrKpFxA44wXGy+/z824EC9L4VvC98WweGELgp6FLyqWyPaZE0u+V0UYc3\nfLQ9sulgO9fP6638pAvrub1qJBWsigYREpfsGD+LOtUCgYEA2Q54EHy2VciL9zch\nvUnfUluAl6qdqTJ5esKLWMj7sViJauREnIbQKa22wEHHNp2d6dTeQrXvJxFY+ITF\nfhWRPzbNwKm+Z24tlzZXlczPHWAW4piu15NbQp04gBY2mn+N2fOn7CCvHAsJf9WE\nF1Wo6jB2PjQMIeklQhThYuIOewcCgYEA113j0mQZBd/fGvPssUKhmupi+PXL8Q+D\nalj2BGL1KNUssYz1m1BRd6Xg8uugEmXSiwb30DlpY6YMh2xe3Q8fjbZ6cgcRYYxG\nj5fxZ5OJApCZ/qDmxhDqWrgzboUh6/Ra58gf9eklZUgxiKjraZHvsB2ZmD2mnX/c\n/D8FOiJt0O8CgYAiESFLBSfhAt7k6CRFf0irtmzSYRbprWBfohUY1EKcrRa0Titu\nncIiKH+cYTaRHd/hCWXw9nkYmqJqLvyIM35ee42DhaC6vxIsv3yzqDeBlslAu1IF\nx7z1EZQClnnvecGbyaA7cU3KivjstWhL+UfPqdeFH8ofsGYGnxxY1r4LFQKBgBm3\nuobbMSnEbA355t/cBxRKvPrhzzLAGX9PlEE91+zCWA3zuTiMxM223z/i50CLLDMx\n59HTqUQfBgv94u4e6jOwALsxIOA3Mg3fpRIrSgcmYV1ItYYdav7eVDX0a/KTQW/A\nNCDXcIHBtziMg2Ad7zNmtXq6kfTo2ei9VdB1YAu9AoGAYvDjtRbe7zc0T9piv8sI\niKigoQJDnpcakRl6f+It1ETOJqTep4DzRE9SmdRg7MySDV57zgNkUOi26rWKbVP9\niXTIs7zGtva7qSMNX6r5844/UnMeZv4ohK8pwMMPXOtVPc3grVx7DX5D2jY3ITid\ncTg/LKJksgROdEz3E7EIQ7k=\n-----END PRIVATE KEY-----',
-            public_key:
-              '-----BEGIN CERTIFICATE-----\nMIIEKzCCAxOgAwIBAgIUTb+JfWjcUWhea7fP1TyqzJIUNVIwDQYJKoZIhvcNAQEL\nBQAwXjELMAkGA1UEBhMCQ04xEzARBgNVBAoTClRlbnBheS5jb20xHTAbBgNVBAsT\nFFRlbnBheS5jb20gQ0EgQ2VudGVyMRswGQYDVQQDExJUZW5wYXkuY29tIFJvb3Qg\nQ0EwHhcNMjMwNTIxMTkzMjM0WhcNMjgwNTE5MTkzMjM0WjCBhDETMBEGA1UEAwwK\nMTYyMTY1Njg4NzEbMBkGA1UECgwS5b6u5L+h5ZWG5oi357O757ufMTAwLgYDVQQL\nDCfmrabmsYnluILmsZ/lpI/ljLrogZrlrrnnvZHnu5zlt6XkvZzlrqQxCzAJBgNV\nBAYMAkNOMREwDwYDVQQHDAhTaGVuWmhlbjCCASIwDQYJKoZIhvcNAQEBBQADggEP\nADCCAQoCggEBALaawkGt7mqv1ntvuEeWKJulR3M8Y+ezEix1aCYZz+5X5sDqjjjP\nIEzUOfBTg5MxWVsDvvnoPCQNwMptYpizWw06ICd618fT7PCJd5roymR4VQe2FHDa\nytLBGrsH3Hu9VCs0JoOciXn55SGCodOWS8nPD+w995CebtQxIfpqSxTQW+QI8a5P\nfGeMaL/ms+iyQkTsGGpvoQ5nVLLhTJTgJL25Uvjat+RWvUWXpvbinm8GDmu0NlJ9\nTiFC/lst2iQE7VTVIVfEYpgJj8yHof7HvunrM3I/qPnLy+2nHdcI69O4k5ygkQyR\n0uPk/Ch8Kuf6cHPSpAi4fezNbCbyFB91i4kCAwEAAaOBuTCBtjAJBgNVHRMEAjAA\nMAsGA1UdDwQEAwID+DCBmwYDVR0fBIGTMIGQMIGNoIGKoIGHhoGEaHR0cDovL2V2\nY2EuaXRydXMuY29tLmNuL3B1YmxpYy9pdHJ1c2NybD9DQT0xQkQ0MjIwRTUwREJD\nMDRCMDZBRDM5NzU0OTg0NkMwMUMzRThFQkQyJnNnPUhBQ0M0NzFCNjU0MjJFMTJC\nMjdBOUQzM0E4N0FEMUNERjU5MjZFMTQwMzcxMA0GCSqGSIb3DQEBCwUAA4IBAQAs\nVwBGYJVIH4kY6K57yL4CGbl5ufhatb/EPM0JABA8LYz3v5agKHMQz5DA86QeRAQ0\n2ceDPBcRIv8NYNxg9gudoeU/vOr4ulVDOYpgQzwdwkoD6qq9akM7JUETdiwHTdU4\nPe9LYVUF9hFJE7iU4UG2FD+aFcLjce5dpYl3zT0m8wZ9FTNQrvsj98dsy4fbndv2\nyRzdY9vpUnKnpH0chHnFzT6BRzmujwtoD5YSqW4BDNHIYzI+7dupwgbtp9dmw7+A\n0hhIiCOXNdeeo92JZUJLp8BsomSNFbHPckunxt99gaQYPYTF+LlBQRUYOP5MnS65\ncOKTpMEZD57/fDqBzbvU\n-----END CERTIFICATE-----',
+            wx_app_id: 'xxx',
+            wx_app_secret: 'xxxxxx',
+            app_id: 'xxx',
+            app_secret: 'xxxxxx',
+            mch_id: 'xxx',
+            mch_key: 'xxxxxx',
+            private_key: 'xxxxxx',
+            public_key: 'xxxxxx',
             status: true
           }
         }
