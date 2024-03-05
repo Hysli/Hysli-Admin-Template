d4671ddd67c75353cbb64bc4a8bb9742d07f7b2e
nightwhite <nightwhite634@gmail.com>
2024-03-05 20:03:39 +0800

    fix: 修复替换API地址脚本 (#5)
    * fix: 修复docker启动命令
    
    * fix: 修复替换命令

--- admin/start.sh
+++ admin/start.sh
@@ -2,8 +2,8 @@
 
 # 替换环境变量
 replace_env_var() {
-  find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s/VITE_GLOB_API_URL:\"\"/VITE_GLOB_API_URL:\"$VITE_GLOB_API_URL\"/g" {} \;
-  find /usr/share/nginx/html -type f -name '*.js' -exec sed -i 's/VITE_GLOB_API_URL:""/VITE_GLOB_API_URL:"'$VITE_GLOB_API_URL'"/g' {} \;
+  find /usr/share/nginx/html -type f -name '*.js' -exec sed -i 's,VITE_GLOB_API_URL:"",VITE_GLOB_API_URL:"'"$VITE_GLOB_API_URL"'",g' {} \;
+  find /usr/share/nginx/html -type f -name '*.js' -exec sed -i 's,"VITE_GLOB_API_URL":"","VITE_GLOB_API_URL":"'"$VITE_GLOB_API_URL"'",g' {} \;
 }
 
 # 在容器启动时执行替换操作
