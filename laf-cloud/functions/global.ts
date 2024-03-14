7d40afcc379f2ff74847a568c8a41f78fdb37893
nightwhite <nightwhite634@gmail.com>
2024-03-14 23:36:27 +0800

    feat: 优化所有云函数引入_ctx都可以正常使用

--- laf-cloud/functions/global.ts
+++ laf-cloud/functions/global.ts
@@ -1,8 +1,26 @@
+import cloud from '@lafjs/cloud'
 import { newFunctionContext } from '@/utils/type'
+import { utils } from '@/utils/index'
+import nw from 'nw-lafjs'
+import console from '@/utils/console'
+const db = cloud.database()
+
 let _ctx: newFunctionContext
 
 export function setCtx(newValue) {
   _ctx = newValue
+}
+
+// 一个用于初始化 _ctx 的函数
+function initializeCtx() {
+  // 在这里编写初始化逻辑
+  const t = new utils.i18n('zhCN').t
+  utils.t = t
+  setCtx(Object.assign(utils, { nw, db, console }))
+}
+
+if (!_ctx) {
+  initializeCtx()
 }
 
 export { _ctx }
